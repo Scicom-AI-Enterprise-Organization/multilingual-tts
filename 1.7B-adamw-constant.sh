f7cdcf749ea925c74b956c9be3c704cab6a9786e
huseinzolkepliscicom <husein.zolkepli@scicom.com.my>
2026-02-04 06:37:54 +0800

    added constant

--- 1.7B-adamw-constant.sh
+++ 1.7B-adamw-constant.sh
@@ -1,13 +1,13 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 WANDB_PROJECT="Multilingual-TTS" \
-WANDB_NAME="Qwen3-1.7B-float32-adamw-linearlr" \
+WANDB_NAME="Qwen3-1.7B-float32-adamw-constantlr" \
 TORCH_DISTRIBUTED_DEBUG="info" \
 torchrun --nproc_per_node 8 \
 -m qwen3_adamw \
 --model_name_or_path "Qwen/Qwen3-1.7B-Base" \
 --per_device_train_batch_size 8 \
 --gradient_accumulation_steps 16 \
---output_dir gfs/01be5b33/Multilingual-TTS-Qwen3-1.7B-float32-adamw-linearlr \
+--output_dir gfs/01be5b33/Multilingual-TTS-Qwen3-1.7B-float32-adamw-constantlr \
 --bf16 --do_train --do_eval false --num_train_epochs 1 \
 --train_file "gfs/01be5b33/combine-multipacking" \
 --logging_steps 1 \
@@ -22,4 +22,4 @@ torchrun --nproc_per_node 8 \
 --dataloader_num_workers 5 \
 --dataloader_prefetch_factor 20 \
 --remove_unused_columns false \
---lr_scheduler_type "linear"
+--lr_scheduler_type "constant"
--- README.md
+++ README.md
@@ -10,9 +10,9 @@ Building actual open source including dataset Multilingual TTS more than 150 lan
 
 ## Ablation
 
-1. Use approximate of 10240 * 128 global token size.
+1. Use approximate of 10240 * 128 * 8 GPUs global token size, ~10,485,760 tokens.
 2. 1 epoch.
 3. learning rate 1e-4.
 4. Warmup step is 100.
-5. Compare AdamW with linear learning rate, AdamW with WSD, Muon + AdamW with WSD, where WSD number decay step is 10% of the dataset.
+5. Compare AdamW with constant learning rate, AdamW with linear decay learning rate, AdamW with WSD learning rate, Muon + AdamW with WSD learning rate, where WSD number decay step is 10% of the dataset.
 6. Only done on Qwen3 1.7B Base.
